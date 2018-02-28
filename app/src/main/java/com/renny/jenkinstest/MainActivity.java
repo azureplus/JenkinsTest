3174a8b3fd0ed961956ce5c86e3d555fed0f15dd
任嘉楠 <7311868400@qq.com>
2018-02-28 22:35:00 +0800

    walle+++++++

--- app/src/main/java/com/renny/jenkinstest/MainActivity.java
+++ app/src/main/java/com/renny/jenkinstest/MainActivity.java
@@ -1,7 +1,10 @@
 package com.renny.jenkinstest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.meituan.android.walle.WalleChannelReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String channel = WalleChannelReader.getChannel(this.getApplicationContext());
+
+        TextView textView=findViewById(R.id.text);
+        textView.setText("channel"+channel);
     }
 }
