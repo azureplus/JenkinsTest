906fd46fa216a07319414676e7433a7d28a402a9
任嘉楠 <7311868400@qq.com>
2018-03-09 17:17:14 +0800

    walle打包

--- tool_output/channel_apk.sh
+++ tool_output/channel_apk.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -jar walle-cli-all.jar batch -f ../markets.txt  outapk/app-ceshi-debug_7zip_aligned_signed.apk  channels
+java -jar walle-cli-all.jar batch -f ../markets.txt  outapk/app-release-debug_7zip_aligned_signed.apk  channels
